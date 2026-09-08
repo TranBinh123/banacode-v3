30fdf65898096e7be94326df7c0b86b3ffda7b8e
Duong Phan Tran Binh <51810396+TranBinh123@users.noreply.github.com>
2026-09-08 21:54:58 +0700

    Update warmupStore.ts

--- src/modules/warmup/store/warmupStore.ts
+++ src/modules/warmup/store/warmupStore.ts
@@ -1,73 +1,56 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import type { TeamId } from "../../../core/types/game";
-import type {
-  QuestionSet,
-  QuestionStatus,
-} from "../types/warmup";
+import type { QuestionSet, QuestionStatus } from "../types/warmup";
 import { DEFAULT_QUESTION_SETS } from "../data/defaultQuestionSets";
 
 type WarmupStore = {
   questionSets: QuestionSet[];
 
-  setQuestionSets: (
-    sets: QuestionSet[],
-  ) => void;
-
-  addSet: (
-    set: QuestionSet,
-  ) => void;
-
-  updateSet: (
-    set: QuestionSet,
-  ) => void;
-
-  deleteSet: (
-    setId: string,
-  ) => void;
+  setQuestionSets: (sets: QuestionSet[]) => void;
+  addSet: (set: QuestionSet) => void;
+  updateSet: (set: QuestionSet) => void;
+  deleteSet: (setId: string) => void;
 };
 
-export const useWarmupStore =
-  create<WarmupStore>()(
-    persist(
-      (set) => ({
-        questionSets: DEFAULT_QUESTION_SETS,
+export const useWarmupStore = create<WarmupStore>()(
+  persist(
+    (set) => ({
+      questionSets: DEFAULT_QUESTION_SETS,
 
-        setQuestionSets: (questionSets) =>
-          set({ questionSets }),
+      setQuestionSets: (questionSets) =>
+        set({ questionSets }),
 
-        addSet: (questionSet) =>
-          set((state) => ({
-            questionSets: [
-              ...state.questionSets,
-              questionSet,
-            ],
-          })),
+      addSet: (questionSet) =>
+        set((state) => ({
+          questionSets: [
+            ...state.questionSets,
+            questionSet,
+          ],
+        })),
 
-        updateSet: (questionSet) =>
-          set((state) => ({
-            questionSets:
-              state.questionSets.map(
-                (item) =>
-                  item.id === questionSet.id
-                    ? questionSet
-                    : item,
-              ),
-          })),
+      updateSet: (questionSet) =>
+        set((state) => ({
+          questionSets: state.questionSets.map(
+            (item) =>
+              item.id === questionSet.id
+                ? questionSet
+                : item,
+          ),
+        })),
 
-        deleteSet: (setId) =>
-          set((state) => ({
-            questionSets:
-              state.questionSets.filter(
-                (item) => item.id !== setId,
-              ),
-          })),
-      }),
-      {
-        name: "olympia-warmup-config",
-      },
-    ),
-  );
+      deleteSet: (setId) =>
+        set((state) => ({
+          questionSets: state.questionSets.filter(
+            (item) => item.id !== setId,
+          ),
+        })),
+    }),
+    {
+      name: "olympia-warmup-config",
+    },
+  ),
+);
 
 export const QUESTION_STATUSES: QuestionStatus[] = [
   "unanswered",
